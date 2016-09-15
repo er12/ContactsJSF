82fb161d39a9574a24ff9bf63a603021dbb21d0f
EdmilDM <edmildm@gmail.com>
2016-09-14 23:00:27 -0400

    Arreglando configuracion.

---
+++ src/Bean/Bean.java
@@ -0,0 +1,15 @@
+package Bean;
+
+/**
+ * Created by edmildimassimo on 9/14/16.
+ */
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@javax.faces.bean.ManagedBean
+@SessionScoped
+public class Bean {
+
+}
